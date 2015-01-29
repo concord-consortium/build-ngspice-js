6e8f6c6ae0f327a5dc0981b4c7b9e24339fccc9e
Richard Klancer <rpk@pobox.com>
2015-01-29 14:15:28 -0500

    Demonstrate async writing to ngspice console

--- pre.js
+++ pre.js
@@ -15,14 +15,33 @@ OutStream.prototype.accept = function(charCode) {
 };
 
 Module.preRun.push(function() {
-	var inputs = ['help', 'source test.cir', 'run', 'print V(1)', 'quit'].join('\n').split('');
+	var inputs = ['help\n'].join('\n').split('');
 	function input() {
 		var chr = inputs.shift();
-		return chr ? chr.charCodeAt() : null;
+		if (!chr) {
+			// If there's no more input, just pretend we pressed enter.
+			//
+			// Currently emscripten doesn't support nonblocking streams. If we return null
+			// then emscripten's "stdin" stream is permanently at EOF. If we return undefined
+			// then emscripten sets EAGAIN and permanently marks the stream as having errored
+			// (All subsequent calls to getc() will return -1 once we return undefined from 
+		    // input().)
+			// However, with a slight tweak to the ngspice source, reading '\n' from stdin
+			// simply quits the mainloop until the emscripten interval calls it back again.
+			return 10;
+		}
+		return chr.charCodeAt();
 	}
 	var stdout = new OutStream();
 	var stderr = new OutStream();
 
+	// Demonstration of how we can asynchronously control ngspice-js
+	window.setTimeout(function() {
+		console.log("(asynchronously appending to stdin)");
+		inputs = inputs.concat(['source test.cir', 'run', 'print V(1)'].join('\n').split(''));
+	}, 1000);
+
+	// Initialize stdin, stdout, and stderr
 	FS.init(input, stdout.accept.bind(stdout), stderr.accept.bind(stderr));
 });
 
